14940bdbd7c82ff062d3755c2e7220ea9e46b262
flylikeahokie <nttalp@gmail.com>
2025-08-13 16:21:27 -0500

    added error handling for blank prompt and added verbose input handling

--- main.py
+++ main.py
@@ -7,19 +7,35 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 from google import genai
+from google.genai import types
 
 client = genai.Client(api_key=api_key)
-#model = "gemini-2.0-flash-001"
 
 def main():
     #print("Hello from my-ai-agent!")
+    if len(sys.argv) < 2 :
+        print("an error has orrcurred, please enter a prompt")
+        sys.exit(1)
+    user_prompt = sys.argv[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
     response = client.models.generate_content(
         model="gemini-2.0-flash-001", 
-        contents="Why is the sun hot?"
-        )
+        contents=messages,
+    )
+    #elif "--verbose" in sys.argv:
     print("Response:")
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+"""print("Response:")
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")"""
     
 if __name__ == "__main__":
     main()
